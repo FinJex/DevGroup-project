de2e2828abb06f8b51e52532509f0abe81965dba
Natalia Filtanovych <natashe4ka21092016@gmail.com>
2026-04-05 23:01:38 +0300

    add izitoast, fix loadMoreBtn

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,12 +1,6 @@
 import { refs } from './refs';
-
-export function toggleLoadMoreBtn() {
-  if (page >= totalPages) {
-    refs.loadMoreBtn.classList.add('hidden');
-  } else {
-    refs.loadMoreBtn.classList.remove('hidden');
-  }
-}
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 export function showLoader() {
   refs.loader.classList.remove('hidden');
@@ -14,4 +8,42 @@ export function showLoader() {
 
 export function hideLoader() {
   refs.loader.classList.add('hidden');
+}
+
+export function showLoadMoreBtn() {
+  refs.loadMoreBtn.classList.remove('hidden');
+}
+
+export function hideLoadMoreBtn() {
+  refs.loadMoreBtn.classList.add('hidden');
+}
+
+export function showToast(message, type = 'success') {
+  const options = {
+    message,
+    position: 'bottomRight',
+    timeout: 5000,
+  };
+
+  switch (type) {
+    case 'success':
+      iziToast.success(options);
+      break;
+    case 'error':
+      iziToast.error(options);
+      break;
+    case 'warning':
+      iziToast.warning(options);
+      break;
+    case 'info':
+      iziToast.info(options);
+      break;
+    default:
+      iziToast.error({
+        message: `Invalid type of toast: ${type}`,
+        position: 'topRight',
+        timeout: 5000,
+      });
+      break;
+  }
 }
