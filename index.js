85ee860cbb53be4a3f1c2553bd4e533750cba8db
FinJex <maks.vasylovych12@gmail.com>
2026-04-05 14:11:18 +0000

    Deploying to gh-pages from @ FinJex/DevGroup-project@b787b3249c1482fb9379aaca622b3d26580cea46 🚀

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-import{a as l}from"./assets/vendor-B2N6ulqC.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const u of o.addedNodes)u.tagName==="LINK"&&u.rel==="modulepreload"&&n(u)}).observe(document,{childList:!0,subtree:!0});function r(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function n(t){if(t.ep)return;t.ep=!0;const o=r(t);fetch(t.href,o)}})();const h="https://furniture-store-v2.b.goit.study/api",L={CATEGORIES:"/categories",PRODUCTS:"/furnitures",ID:"/furnitures/{id}",FEEDBACKS:"/feedbacks"},p=8;l.defaults.baseURL=h;async function $(){return(await l(`${L.CATEGORIES}`)).data}async function d(e){return(await l(`/furnitures?page=${e}&limit=${p}`)).data}async function m(e,a){return(await l(`/furnitures?page=${a}&limit=${p}&category=${e}`)).data}const c={categories:document.querySelector(".categories-list"),furnitureList:document.querySelector(".furniture-list"),loadMoreBtn:document.querySelector(".load-more"),backdrop:document.querySelector(".backdrop"),modal:document.querySelector(".modal"),loader:document.querySelector(".loader")},f={"М'які меблі":"sofa.jpg","Шафи та системи зберігання":"wardrobe.jpg","Ліжка та матраци":"bed.jpg",Столи:"table.jpg","Стільці та табурети":"chairs.jpg",Кухні:"kitchen.jpg","Меблі для дитячої":"childrens-room.jpg","Меблі для офісу":"home-office.jpg","Меблі для передпокою":"entryway.jpg","Меблі для ванної кімнати":"bathroom.jpg","Садові та вуличні меблі":"outdoor-patio.jpg","Декор та аксесуари":"home-decor.jpg",all:"showroom.jpg"};function j(e){const a=[`<li>
-    <button class="category-btn active"
-            data-id="all"
-            data-bg="${f.all}">
-      Всі товари
-    </button>
-  </li>`,...e.map(n=>`
-      <li>
-        <button class="category-btn"
-                data-id="${n._id}"  
-                data-bg="${f[n.name]}">
-          ${n.name}
-        </button>
-      </li>
-    `)].join("");c.categories.innerHTML=a,c.categories.querySelectorAll(".category-btn").forEach(n=>{const t=`../img/furniture-list/${n.dataset.bg}`,o=t.replace(/\.jpg$/,"@2x.jpg");n.style.backgroundImage=`url(${t})`,window.devicePixelRatio>1&&(n.style.backgroundImage=`url(${o})`)})}const E={Cірий:"#c7c3bb",Жовтий:"#c7aa80",Чорний:"#201a19"};function g(e){const a=e.map(r=>{var t;const n=(r.colors||r.color||[]).slice(0,3);return console.log(r),`
-     <li class="furniture-card" data-id="${r._id}">
-       <img src="${(t=r.images)==null?void 0:t[0]}" alt="${r.name}">
-       <h3 class="furniture-name">${r.name}</h3>
+import{a as l}from"./assets/vendor-B2N6ulqC.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))s(r);new MutationObserver(r=>{for(const a of r)if(a.type==="childList")for(const u of a.addedNodes)u.tagName==="LINK"&&u.rel==="modulepreload"&&s(u)}).observe(document,{childList:!0,subtree:!0});function t(r){const a={};return r.integrity&&(a.integrity=r.integrity),r.referrerPolicy&&(a.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?a.credentials="include":r.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function s(r){if(r.ep)return;r.ep=!0;const a=t(r);fetch(r.href,a)}})();const b="https://furniture-store-v2.b.goit.study/api",L={CATEGORIES:"/categories",PRODUCTS:"/furnitures",ID:"/furnitures/{id}",FEEDBACKS:"/feedbacks"},g=8;l.defaults.baseURL=b;async function h(){return(await l(`${L.CATEGORIES}`)).data}async function d(e){return(await l(`/furnitures?page=${e}&limit=${g}`)).data}async function y(e,o){return(await l(`/furnitures?page=${o}&limit=${g}&category=${e}`)).data}const c={categories:document.querySelector(".categories-list"),furnitureList:document.querySelector(".furniture-list"),loadMoreBtn:document.querySelector(".load-more"),backdrop:document.querySelector(".backdrop"),modal:document.querySelector(".modal"),loader:document.querySelector(".loader")};function $(e){const o=[`<li>
+     <button class="category-btn active" data-id="all" data-category="all">
+       Всі товари
+     </button>
+   </li>`,...e.map(t=>`
+     <li>
+       <button class="category-btn"
+               data-id="${t._id}"
+               data-category="${t.name}">
+         ${t.name}
+       </button>
+     </li>
+   `)].join("");c.categories.innerHTML=o}const E={Cірий:"#c7c3bb",Жовтий:"#c7aa80",Чорний:"#201a19"};function f(e){const o=e.map(t=>{var r;const s=(t.colors||t.color||[]).slice(0,3);return`
+     <li class="furniture-card" data-id="${t._id}">
+       <img src="${(r=t.images)==null?void 0:r[0]}" alt="${t.name}">
+       <h3 class="furniture-name">${t.name}</h3>
        <div class="color-list">
-         ${n.map(o=>`
+         ${s.map(a=>`
            <span
              class="color-dot"
-             style="background:${E[o]||o}"
-             title="${o}"
+             style="background:${E[a]||a}"
+             title="${a}"
            ></span>
          `).join("")}
        </div>
-       <p class="furniture-price">${r.price} грн</p>
+       <p class="furniture-price">${t.price} грн</p>
        <button class="details-btn">Детальніше</button>
      </li>
-   `}).join("");c.furnitureList.insertAdjacentHTML("beforeend",a)}let s=1,i="all",y=1;async function S(){s=1;try{const e=await $();console.log(e);const a=["Всі товари",...e];j(e);const{furnitures:r,totalItems:n}=await d(s);console.log(r,n),g(r)}catch(e){console.log(e)}}async function w(e){const a=e.target.closest(".category-btn");if(a){s=1,i=a.dataset.id;try{document.querySelectorAll(".category-btn").forEach(o=>{o.classList.remove("active")}),a.classList.add("active"),c.furnitureList.innerHTML="";let r;i==="all"?r=await d(s):r=await m(i,s);const{furnitures:n,totalItems:t}=r;y=Math.ceil(t/8),g(n),b()}catch(r){console.log(r)}}}async function P(){s+=1;try{let e;i==="all"?e=await d(s):e=await m(i,s);const{furnitures:a}=e;g(a),b()}catch(e){console.log(e)}}function b(){s>=y?c.loadMoreBtn.classList.add("hidden"):c.loadMoreBtn.classList.remove("hidden")}document.addEventListener("DOMContentLoaded",S);c.categories.addEventListener("click",w);c.loadMoreBtn.addEventListener("click",P);
+   `}).join("");c.furnitureList.insertAdjacentHTML("beforeend",o)}let n=1,i="all",m=1;async function S(){n=1;try{const e=await h();console.log(e);const o=["Всі товари",...e];$(e);const{furnitures:t,totalItems:s}=await d(n);console.log(t,s),f(t)}catch(e){console.log(e)}}async function M(e){const o=e.target.closest(".category-btn");if(o){n=1,i=o.dataset.id;try{document.querySelectorAll(".category-btn").forEach(a=>{a.classList.remove("active")}),o.classList.add("active"),c.furnitureList.innerHTML="";let t;i==="all"?t=await d(n):t=await y(i,n);const{furnitures:s,totalItems:r}=t;m=Math.ceil(r/8),f(s),p()}catch(t){console.log(t)}}}async function P(){n+=1;try{let e;i==="all"?e=await d(n):e=await y(i,n);const{furnitures:o}=e;f(o),p()}catch(e){console.log(e)}}function p(){n>=m?c.loadMoreBtn.classList.add("hidden"):c.loadMoreBtn.classList.remove("hidden")}document.addEventListener("DOMContentLoaded",S);c.categories.addEventListener("click",M);c.loadMoreBtn.addEventListener("click",P);
 //# sourceMappingURL=index.js.map
